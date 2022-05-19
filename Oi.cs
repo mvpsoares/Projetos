60198997a4b0e32dfcce16de05d3c5e2c83ffea1
Marcellus Pinheiro Soares <soares.mvp@gmail.com>
2022-05-18 21:56:57 -0300

    config c#

---
+++ Oi.cs
@@ -0,0 +1,10 @@
+using System;
+namespace Projetos
+{
+    public class Oi
+    {
+        public void BoaNoite(){
+            Console.WriteLine("Boa Noite!");
+        }
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+
+namespace Projetos
+{
+    
+    class Program 
+    {
+        
+        static void Main(string[] args) 
+        {
+            Oi oi = new Oi();
+            Console.WriteLine("Hello, World!");
+
+            oi.BoaNoite();
+        }
+    }
+}
+
+
+
